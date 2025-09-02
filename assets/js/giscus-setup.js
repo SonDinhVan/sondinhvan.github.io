987cf5b7ca70799c728c279464214597706c5b20
SonDinhVan <sondv89@gmail.com>
2025-09-02 20:29:50 +0000

    Deploying to gh-pages from @ SonDinhVan/sondinhvan.github.io@883fdb6eea9f212e8b2c92894d71bf83a6bbfc4a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SonDinhVan/academic_website",
+    "data-repo": "SonDinhVan/sondinhvan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
